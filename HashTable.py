c7c6ab935b8fd5ef7ae834eaf46e42917c02fc5d
Merissa Bridgeman <merissa.bridgeman98@gmail.com>
2021-02-18 21:10:18 -0800

    created the starter code

---
+++ HashTable.py
@@ -0,0 +1,16 @@
+from LinkedList import LinkedList
+
+
+class HashTable:
+
+    def __init__(self, size):
+        self.size = size
+        #self.arr = self.create_arr(size)
+
+    # 1️⃣ TODO: Complete the create_arr method.
+
+    # Each element of the hash table (arr) is a linked list.
+    # This method creates an array (list) of a given size and populates each of its elements with a LinkedList object.
+
+    def create_arr(self, size):
+        pass
